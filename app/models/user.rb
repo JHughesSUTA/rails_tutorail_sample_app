2b7a29486691f44a356af7f3127f4d640aa869c6
James Hughes <james.a.hughes@gmail.com>
2018-03-13 10:40:15 -0500

    Add secure password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,13 @@
 class User < ApplicationRecord
   # change to lowercase before saving to db, for purpose of case-sensitive indeces
   before_save { self.email = email.downcase } # could be = self.email.downcase, but self is optional on the right hand side 
-  # could also be: before_save {email.downcase!} 
+  # could also be: before_save {email.downcase!}
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }   # validates uniqueness, but case insensitive
-end
-
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }   # validates uniqueness, but case insensitive
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
