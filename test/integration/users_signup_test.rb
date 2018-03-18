4ef5cf6f8630e7ec76da1f8e5222d05e3392cb6e
James Hughes <james.a.hughes@gmail.com>
2018-03-18 14:02:23 -0500

    Handle successful signup, finsish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,4 +15,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
     assert_select 'form[action="/signup"]'  # tests that submitted form goes to /signup rather than /users
   end
+
+  test "valid signup information" do 
+    get signup_path
+    assert_difference 'User.count', 1 do    # 2nd argument is option, and means what the difference should be (1 difference)
+      post users_path, params: { user: { name: "Example User",
+                                         email: 'user@example.com',
+                                         password: "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!    # arranges to follow the redirect after submission
+    assert_template 'users/show'
+    assert_not flash.nil?   # tests that the flash message appears
+  end
 end
