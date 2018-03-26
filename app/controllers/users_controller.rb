5c7c34561d2839e5dd73928a9a594e396b40826e
James Hughes <james.a.hughes@gmail.com>
2018-03-26 14:37:25 -0500

    Complete users index test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page]) # parmas[:page] generated automatically by will_paginate
   end
 
   def new
