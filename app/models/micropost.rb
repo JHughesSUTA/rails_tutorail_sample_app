7c5f3c1d0729feea9feb4c25926f540a3a6dc369
James Hughes <james.a.hughes@gmail.com>
2018-04-02 09:56:49 -0500

    Validate uploaded image

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,4 +5,15 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader    # adds the uploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size  # custom validation uses 'validate'
+
+  private
+
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
+    
 end
